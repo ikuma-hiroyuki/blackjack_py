944a2a35f8c1a09545c837aec5bc59a66bc590eb
ikuma <chemicalbondo@gmail.com>
2023-11-07 15:24:00 +0900

    スコア計算メソッドを実装

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import random
 
+from card import Card
 from deck import Deck
 
 
@@ -7,37 +8,63 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.score = 0
-        self.hand = []
+        self.is_stand = False
+        self.hand: list[Card] = []
 
     def hit(self):
         """カードを1枚引く"""
+        random_index = random.randint(0, len(deck.card_list) - 1)
+        self.hand.append(deck.card_list.pop(random_index))
 
     def stand(self):
-        """カードを引かずにターンを終了する"""
+        """スタンドしてスコアを確定させる"""
+        self.is_stand = True
+        self.calculate_score()
 
     def show_hand(self):
         """手札を表示する"""
         print(f'{self.name}の手札: ', end='')
         for card in self.hand:
             print(card, end=' ')
-        print()
+
+    def calculate_score(self):
+        """手札のスコアを計算する"""
+        initial_score = sum(card.score for card in self.hand)
+        ace_count = sum(1 for card in self.hand if card.is_ace)
+
+        adjusted_score = initial_score
+        while adjusted_score > 21 and ace_count > 0:
+            adjusted_score -= 10
+            ace_count -= 1
+        self.score = adjusted_score
 
 
 class User(Player):
-    def hit(self):
-        """カードを1枚引く"""
-        random_index = random.randint(0, len(deck.card_list) - 1)
-        self.hand.append(deck.card_list.pop(random_index))
+    def __init__(self):
+        super().__init__('ユーザー')
+        self.money = 0
 
 
 if __name__ == '__main__':
     deck = Deck()
-    user = User('ユーザー')
+    user = User()
+
     while True:
         is_hit = input('Hit? (y/n)')
         if is_hit == 'y':
             user.hit()
             user.show_hand()
+            print()
             print(deck.card_list)
         else:
+            user.stand()
+            print(user.score)
             break
+
+    A1 = Card('♥', 'A', {'score': 11, 'is_ace': True})
+    A2 = Card('♠', 'A', {'score': 11, 'is_ace': True})
+    S10 = Card('♠', '10', {'score': 10, 'is_ace': False})
+    user.hand = [A1, A2, S10]
+    user.stand()
+    user.show_hand()
+    print(user.score)
