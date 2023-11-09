4826720301d6c05cfa98b1789610b0a33af0c4bf
ikuma <chemicalbondo@gmail.com>
2023-11-09 15:57:30 +0900

    インスタンス化時にカードをシャッフルするようにした

--- deck.py
+++ deck.py
@@ -1,17 +1,24 @@
+from random import shuffle
+
 from card import Card
 
 
 class Deck:
-    """デッキを表すクラス"""
+    """
+    デッキを表すクラス
+
+    インスタンス化と同時にデッキ(ジョーカーを除く52枚のカード)を作り、シャッフルする
+    """
 
     def __init__(self):
         self.card_list = []
-        self._create_deck()
+        self._create_deck_and_shuffle()
 
-    def _create_deck(self):
+    def _create_deck_and_shuffle(self):
         for suit in Card.SUITS:
             for rank, score in Card.CARD_SCORE.items():
                 self.card_list.append(Card(suit, rank))
+        shuffle(self.card_list)
 
 
 if __name__ == '__main__':
