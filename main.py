4a0e6ffaaae529843b9e7247e208fb32298811f2
ikuma <chemicalbondo@gmail.com>
2023-11-10 17:49:54 +0900

    ゲームロジックを追加

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from enum import Enum, auto
 
 from art_manager import ArtManager
@@ -36,6 +37,10 @@ class GameManager:
 
     def play_game(self):
         """ゲームを開始する"""
+        self._clear_terminal()
+        print(self.art.title)
+        input('エンターキーを押すとカードが配られます:')
+        self._clear_terminal()
         self._play_rounds()
 
     def _play_rounds(self):
@@ -50,7 +55,7 @@ class GameManager:
     def _round_of_game(self):
         """ゲームを1回戦行う"""
 
-        self._ask_bets()
+        # self._ask_bets()
         self._deal_card()
         self._show_initial_hands()
 
@@ -65,39 +70,69 @@ class GameManager:
         """ゲーム開始直後にユーザーとディーラーインスタンスに2枚ずつカードを配る"""
         for _ in range(2):
             for player in self.players:
-                player.hit()
+                player.hand.append(Player.deck.card_list.pop())
+                player.calculate_score()
+
+    def _dealer_should_draw_card(self):
+        """
+        ディーラーがカードを引くべきか判定する
+
+        ユーザーかディーラーがバーストしている場合は引かない
+        ディーラーが17点以上かつユーザーに勝っている場合は引かない
+        ユーザーが21点、ディーラーも21点の場合は引かない
+        ディーラーが17点以上でユーザーに勝っている場合は引かない
+
+        ディーラーが17点未満でもユーザーに勝っている場合は引く
+
+        return: True: カードを引く, False: カードを引かない
+        """
+
+        if self.user.is_burst or self.dealer.is_burst:
+            return False
+        if self.dealer.score >= ScoreRules.DEALER_MIN_VALUE.value and self.dealer.score > self.user.score:
+            return False
+        if (self.user.score == self.dealer.score) and ScoreRules.BLACK_JACK_VALUE.value:
+            return False
+
+        return self.dealer.score < ScoreRules.DEALER_MIN_VALUE.value or self.dealer.score < self.user.score
 
     def _round_for_stand_or_burst(self, player: Player):
         """
         プレイヤーがスタンドもしくはバーストするまで待機する(繰り返す)
         :param player: ユーザーもしくはディーラーインスタンス
         """
-        while player.is_stand or player.is_burst:
-            player.hit()
+
+        while True:
+            if player.is_stand or player.is_burst:
+                break
 
             if isinstance(player, User):
+                if self.user.ask_stand():
+                    player.stand()
+                else:
+                    player.hit()
                 self._show_initial_hands()
 
             if isinstance(player, Dealer):
-                self._show_final_hands()
-
-            self.show_burst_status(player)
+                while self._dealer_should_draw_card():
+                    self._show_final_hands()
+                    input('エンターキーを押してください:')
+                    player.hit()
+                player.stand()
 
     def _show_initial_hands(self):
         """ユーザーの全てのカードを表に、ディーラーの1枚のカードを表にする"""
+        self._clear_terminal()
+        self.user.show_all_face_and_score()
+        self.dealer.show_card_face(num_visible_cards=1)
+        print()
 
     def _show_final_hands(self):
         """ユーザーとディーラーの全てのカードを表にする"""
-
-    def show_burst_status(self, player: Player):
-        """
-        バーストしていればAAを表示する
-        :param player: ユーザーもしくはディーラーインスタンス
-        """
-
-        if player.is_burst:
-            print(f"{player.name}: バースト")
-            print(self.art.burst)
+        self._clear_terminal()
+        self.user.show_all_face_and_score()
+        self.dealer.show_all_face_and_score()
+        print()
 
     def _check_natural_blackjack(self, player: Player):
         """
@@ -105,11 +140,11 @@ class GameManager:
         :param player: ユーザーもしくはディーラーインスタンス
         """
 
-        result = player.score == ScoreRules.BLACK_JACK_VALUE and len(player.hand) == 2
-        if isinstance(player, User):
-            print(f"{player.name}: ブラックジャック！")
+        result = player.score == ScoreRules.BLACK_JACK_VALUE.value and len(player.hand) == 2
+        if isinstance(player, User) and result:
             print(self.art.blackjack)
-            self.user_is_natural_blackjack = result
+            input(f"{player.name}: ブラックジャック！")
+            self.user_state.is_natural_blackjack = result
 
     def _evaluate_judge(self):
         """ユーザーの勝敗を判定し掛け金分配率を決定する"""
@@ -118,22 +153,30 @@ class GameManager:
 
     def _judge_game_state(self):
         """ユーザーの勝敗を判定する"""
+        self._show_final_hands()
+
         if self.user.is_burst:
+            print(self.art.burst)
+            print(self.art.lose)
             return UserGameState.LOSE
         if self.dealer.is_burst:
+            print(self.art.win)
             return UserGameState.WIN
 
         if self.user.score > self.dealer.score:
+            print(self.art.win)
             return UserGameState.WIN
         elif self.user.score == self.dealer.score:
+            print(self.art.draw)
             return UserGameState.DRAW
         else:
+            print(self.art.lose)
             return UserGameState.LOSE
 
     def _judge_distribute_bet(self):
         """掛け金分配率を決定する"""
         if self.user_state.game_result == UserGameState.WIN:
-            return BetRules.NATURAL_BLACK_JACK.value if self.user_is_natural_blackjack else BetRules.WIN.value
+            return BetRules.NATURAL_BLACK_JACK.value if self.user_state.is_natural_blackjack else BetRules.WIN.value
         elif self.user_state.game_result == UserGameState.DRAW:
             return BetRules.DRAW.value
         else:
@@ -142,10 +185,23 @@ class GameManager:
     @staticmethod
     def _is_user_replay_decision():
         """ユーザーに再戦するかどうか尋ねる"""
-        return bool(input('再戦する場合は何か入力してエンターキーを押してください: '))
+        return not bool(input('終了する場合は何か入力してエンターキーを押してください: '))
 
     def _ask_bets(self):
         """掛け金をユーザーに尋ねる"""
+        while True:
+            try:
+                bet_amount = int(input('掛け金を入力してください: '))
+                if bet_amount < 0:
+                    raise ValueError
+                if bet_amount > self.user.money:
+                    raise ValueError
+            except ValueError:
+                print('掛け金は0以上、かつ所持金以下の整数で入力してください')
+            else:
+                self.user_state.bet_amount = bet_amount
+                self.user.money -= bet_amount
+                break
 
     def _distribute_bets(self):
         """掛け金を分配する"""
@@ -156,6 +212,11 @@ class GameManager:
         for player in self.players:
             player.reset_deal()
         self.user_state = UserState()
+
+    @staticmethod
+    def _clear_terminal():
+        """ターミナルをクリアする"""
+        os.system('cls' if os.name == 'nt' else 'clear')
 
 
 if __name__ == '__main__':
