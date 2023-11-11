127a568639220de429dfd627cb8a31665f94781d
ikuma-hiroyuki <chemicalbondo@gmail.com>
2023-11-12 05:38:14 +0900

    微修正

--- tests/test_game_manager.py
+++ tests/test_game_manager.py
@@ -20,7 +20,7 @@ class TestGameManager:
         self.manager.dealer.score = 17
         self.manager.dealer.is_burst = False
 
-        assert self.manager._dealer_should_draw_card() is False
+        assert self.manager.judge_helper.dealer_should_draw_card() is False
 
     def test_dealer_should_draw_under_17_winning(self):
         """ディーラーが17点未満でもユーザーに勝っている場合は引く"""
@@ -29,7 +29,7 @@ class TestGameManager:
         self.manager.dealer.score = 16
         self.manager.dealer.is_burst = False
 
-        assert self.manager._dealer_should_draw_card() is True
+        assert self.manager.judge_helper.dealer_should_draw_card() is True
 
     def test_dealer_should_draw_user_burst(self):
         """ユーザーがバーストしている場合は引かない"""
@@ -37,14 +37,14 @@ class TestGameManager:
         self.manager.user.is_burst = True
         self.manager.dealer.score = 10
 
-        assert self.manager._dealer_should_draw_card() is False
+        assert self.manager.judge_helper.dealer_should_draw_card() is False
 
     def test_dealer_should_draw_burst(self):
         """ディーラーがバーストしている場合は引かない"""
         self.manager.dealer.score = 22
         self.manager.dealer.is_burst = True
 
-        assert self.manager._dealer_should_draw_card() is False
+        assert self.manager.judge_helper.dealer_should_draw_card() is False
 
     def test_dealer_should_draw_losing(self):
         """ディーラーがユーザーに負けている場合は引く"""
@@ -53,7 +53,7 @@ class TestGameManager:
         self.manager.dealer.score = 19
         self.manager.dealer.is_burst = False
 
-        assert self.manager._dealer_should_draw_card() is True
+        assert self.manager.judge_helper.dealer_should_draw_card() is True
 
     def test_dealer_should_draw_tie_at_21(self):
         """ユーザーが21点、ディーラーも21点の場合は引かない"""
@@ -62,7 +62,7 @@ class TestGameManager:
         self.manager.dealer.score = 21
         self.manager.dealer.is_burst = False
 
-        assert self.manager._dealer_should_draw_card() is False
+        assert self.manager.judge_helper.dealer_should_draw_card() is False
 
     def test_dealer_should_draw_tie_at_except_21(self):
         """ディーラーが21点、ユーザーも21点以外で同点の場合は引く"""
@@ -71,4 +71,4 @@ class TestGameManager:
         self.manager.dealer.score = 20
         self.manager.dealer.is_burst = False
 
-        assert self.manager._dealer_should_draw_card() is True
+        assert self.manager.judge_helper.dealer_should_draw_card() is True
